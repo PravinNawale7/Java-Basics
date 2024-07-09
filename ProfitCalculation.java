3c5091f5eba4d01a7445e9f86d8f984c9eb8a75e
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-07-09 09:48:32 +0530

    Profit Calculation

---
+++ ProfitCalculation.java
@@ -0,0 +1,12 @@
+public class ProfitCalculation{
+    public static void main(String[] args){
+     float sellingPrice = 150;
+     float costPrice = 100;
+     float profitPercentage = (sellingPrice-costPrice)/costPrice*100;
+     System.out.println("Selling Price is : "+sellingPrice);
+     System.out.println("Cost Price is : "+costPrice);
+     System.out.println("Profit Percentage  is : "+profitPercentage);
+
+
+    }
+}
