189fe98c3bdd3b16bee073a9e59c534d0878fa2b
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-07-09 09:48:09 +0530

    Prime Number Program

---
+++ PrimeNo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int flag=1;
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                System.out.println("No is not Prime");
+                flag=0;
+                break;
+            }
+    }
+        if(flag==1){
+            System.out.println("No is Prime");
+        }
+    }
+}
+    
