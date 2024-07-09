a56d933a6c6de06730376d943059a603fb394d17
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-07-09 09:51:03 +0530

    Swapping of Two numbers

---
+++ Swap.java
@@ -0,0 +1,16 @@
+public class Swap{
+    public static void main(String[] args) {
+        int a = 10,b=20;
+        // int temp = a;
+        // a = b;
+        // b = temp;
+        // System.out.println("Value of a is "+a+" Value of b is "+b); 
+        a=a+b;
+        b=a-b;
+        a=a-b;
+       System.out.println("Value of a is "+a+" Value of b is "+b); 
+
+    
+    
+    }
+}
