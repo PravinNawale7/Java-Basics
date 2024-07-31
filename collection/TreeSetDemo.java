96f1a5bccf2ac2cf4fe463ce7b0feaf32f91ae97
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-07-31 09:55:15 +0530

    TresSet Program in Collection
    This program demonstrate the TreeSet Program in Java

---
+++ collection/TreeSetDemo.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet();
+		ts.add(30);
+		ts.add(60);
+		ts.add(10);
+		ts.add(5);
+		System.out.println(ts);
+		
+		
+	}
+}
