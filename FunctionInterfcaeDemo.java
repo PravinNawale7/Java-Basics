8840f6dba02382d2ad8ab21bd46e842a40ed8f59
pravin-nawale <159033956+pravin-nawale@users.noreply.github.com>
2024-07-31 10:17:24 +0530

    Lambda Expression Program
    In this program demonstrate the functional interface using lamba function

---
+++ FunctionInterfcaeDemo.java
@@ -0,0 +1,8 @@
+
+public class FunctionInterfcaeDemo {
+	public static void main(String[] args) {
+		   Functional_Interface functional = (a, b) -> a + b;
+		   int result = functional.add(10, 20);
+		   System.out.println("Result: " + result);
+		    }
+	}
